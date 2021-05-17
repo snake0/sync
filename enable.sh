85fb017b7bf4f5efe88e96e693c6ef8f6743e57e
snake0 <1260865816@qq.com>
2021-05-17 15:17:18 +0800

    xmrig

---
+++ enable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://xmrig.com/docs/miner/hugepages#onegb-huge-pages
+
+sysctl -w vm.nr_hugepages=$(nproc)
+
+for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
+do
+    echo $(nproc) > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
+done
+
+echo "1GB pages successfully enabled"
+./xmrig
+
